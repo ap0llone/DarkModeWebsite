77430150f00778f6a7d5b26a90c902a88595167f
Abbas Ibragimov <120020090+ap0llone@users.noreply.github.com>
2023-01-08 15:50:59 +0400

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,25 @@
+//dark theme
+const btn = document.getElementById("btn");
+const btnText = document.getElementById("btnText");
+const btnIcon = document.getElementById("btnIcon");
+
+btn.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    
+    if(document.body.classList.contains("dark-theme")){
+        btnIcon.src = "style/images/sun.png";
+        btnText.innerHTML = "Light";
+    }else{
+        btnIcon.src = "style/images/moon.png"
+        btnText.innerHTML = "Dark";
+    }
+}
+
+//navbar hamburger
+var menu = document.querySelector('#menu-icon');
+var navBar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navBar.classList.toggle('open');
+}
